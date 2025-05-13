eee3f42faa05194e154efe63eeeeaacf8f98cb5d
jannahnah <jannah.ada@urios.edu.ph>
2025-05-13 05:14:03 +0000

    Initial commit

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .shop_models import Category
-
-admin.site.register(Category)
--- app/api/urls.py
+++ app/api/urls.py
@@ -1,27 +1,16 @@
 from django.urls import path
-from .views import Students, ContactListView, ContactUpdateDetailView
+#from .views import HelloWorld
+from .views import Students
+from .views import ContactListView
+from .views import ContactUpdateDetailView
 from .exam_views import ChatView
-from .shop_views import ProductView, CartView, CheckoutView, CategoryView
 
 urlpatterns = [
-    # Student endpoints
+    #path('hello/', HelloWorld.as_view(), name='hello_world'),
     path('students/', Students.as_view(), name='list_of_students'),
-    
-    # Contact endpoints
     path('contact/', ContactListView.as_view(), name='contact_new'),
     path('contact/<int:contact_id>/', ContactListView.as_view(), name='contact_detail'),
     path('contacts/', ContactListView.as_view(), name='contact_list'),
     path('contacts/<int:contact_id>/', ContactUpdateDetailView.as_view(), name='contact_update_detail'),
-    
-    # Chat endpoint
     path('chat/', ChatView.as_view(), name='chat_view'),
-    
-    # Shop endpoints (fixed)
-    path('shop/', ProductView.as_view(), name='product_view'),
-    path('products/', ProductView.as_view(), name='product_list'),
-    path('cart/', CartView.as_view(), name='cart_view'),  # Changed from chat/
-    path('checkout/', CheckoutView.as_view(), name='checkout_view'),  # Changed from chat/
-    
-    # Category endpoint
-    path('categories/', CategoryView.as_view(), name='category_list'),
 ]
